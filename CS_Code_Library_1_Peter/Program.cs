22aa1b31bf0d40925a29c22b45591de1af9daee9
Peter Sigurdson <peter@petersigurdson.net>
2018-07-25 11:32:21 -0400

    Add project files.

---
+++ CS_Code_Library_1_Peter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Code_Library_1_Peter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
